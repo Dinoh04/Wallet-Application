10590b4b6cc6272b86cf6e1c136d92a10b65cbf6
JeanMarc <jeanmarc.rajaonarivelona01@gmail.com>
2023-12-14 10:38:47 +0300

    fix: Delete update method inCrudOperations interface

--- Wallet_application/src/main/java/org/example/DAO/CrudOperations.java
+++ Wallet_application/src/main/java/org/example/DAO/CrudOperations.java
@@ -3,11 +3,11 @@ package org.example.DAO;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface CrudOperations <T> {
-    public List<T> findAll() throws SQLException;
-    public List<T> saveAll(List<T> toSave)throws SQLException;
-    public T save(T toSave)throws SQLException;
-    public List<T> update(List<T> toUpdate) throws SQLException;
+public interface CrudOperations<T> {
+  public List<T> findAll() throws SQLException;
 
-    public T delete (T toDelete)throws SQLException;
+  public List<T> saveAll(List<T> toSave) throws SQLException;
+
+  public T save(T toSave) throws SQLException;
+  public T delete(T toDelete) throws SQLException;
 }
